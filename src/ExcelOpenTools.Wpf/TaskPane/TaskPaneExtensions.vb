2c07e4cf59b740a9abe0de1af6d3bcadc31da2e4
HbC <hbcsoft@outlook.com>
2021-04-26 23:43:37 +0100

    Projects updated to Sdk="Microsoft.NET.Sdk"

---
+++ src/ExcelOpenTools.Wpf/TaskPane/TaskPaneExtensions.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+Imports System.Windows.Controls
+
+Namespace TaskPane
+
+    ''' <summary>
+    ''' Extension methods for <see cref="WpfTaskPane"/>.
+    ''' </summary>
+    <ComVisible(False)>
+    Public Module TaskPaneExtensions
+
+        ''' <summary>
+        ''' Show the current user control in the task pane.
+        ''' </summary>
+        ''' <param name="userControl">The <see cref="UserControl"/> to show in the task pane.</param>
+        ''' <param name="title">The title to use in the task pane.</param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function ShowInTaskPane(ByVal userControl As UserControl, ByVal title As String) As WpfTaskPane
+
+            'Create and return the WpfTaskPane
+            Return New WpfTaskPane(title:=title) With {.Visible = True, .UserControl = userControl}
+        End Function
+
+    End Module
+
+End Namespace
